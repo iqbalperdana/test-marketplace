66bba0d9dddca4588dbec04cdd471bf2f145ec0a
Iqbal Perdana <iqbalperdana@live.com>
2025-02-10 10:51:27 +0700

    Fix product model: change name field to title, remove unused import

--- app/models/product.py
+++ app/models/product.py
@@ -1,11 +1,10 @@
 from main import db
-from sqlalchemy.orm import declarative_base
 
 class Product(db.Model):
     __tablename__ = 'products'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
+    title = db.Column(db.String(50), nullable=False)
     description = db.Column(db.String(255))
     price = db.Column(db.Float, nullable=False)
     seller_id = db.Column(db.Integer, nullable=False)
