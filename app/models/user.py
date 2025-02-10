139bde4d1981ec19eb66a0bd7219e7cf312e7915
Iqbal Perdana <iqbalperdana@live.com>
2025-02-10 10:52:33 +0700

    Fix user model: add password field for model definition

--- app/models/user.py
+++ app/models/user.py
@@ -1,11 +1,12 @@
 from main import db
 
-class User:
+class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
 
     def __init__(self, username, email, password):
         self.username = username
